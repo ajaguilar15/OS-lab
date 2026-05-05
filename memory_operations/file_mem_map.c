0ebe0412b65ba73eb414674ea4751d5290473790
ajaguilar15 <ajaguilar920@gmail.com>
2026-05-04 21:46:32 -0500

    Implemented writing output to a file

--- memory_operations/file_mem_map.c
+++ memory_operations/file_mem_map.c
@@ -12,8 +12,11 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: %s filename\n", argv[0]);
         return 1;
     }
+
+    char *input_filename = argv[1];
+    char *output_filename = argv[2];
     
-    int fd = open(argv[1], O_RDONLY);
+    int fd = open(input_filename, O_RDONLY);
 
     if (fd == -1)
     {
@@ -32,6 +35,13 @@ int main(int argc, char *argv[])
 
     size_t file_size = file_info.st_size;
 
+    if (file_size == 0)
+    {
+        fprintf(stderr, "Error: input file is empty.\n");
+        close(fd);
+        return 1;
+    }
+
     char *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
     if (data == MAP_FAILED)
@@ -41,21 +51,36 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("File size: %zu bytes\n", file_size);
-    printf("Mapped file starts at virtual address: %p\n\n", (void *)data);
+    FILE *out = fopen(out_filename, "w")
 
-    printf("Reading file every 4096 bytes:\n\n");
-
-    for (size_t i = 0; i < file_size; i += 4096)
+    if (out == NULL)
     {
-        printf("data[%5zu] address = %p, value = %02X\n",
-            i,
-            (void *)&data[i],
-            (unsigned char)data[i]);
+        perro("fopen")
+        munmap(data, file_size);
+        close(fd);
+        return 1;
+    }
+    
+    fprintf(out, "Input file: %s\n", input_filename);
+    fprintf(out, "File size: %zu bytes\n", file_size);
+    fprintf(out, "Mapped file starts at virtual address: %p\n\n", (void *)data);
+
+    fprintf(out, "Reading file every %d bytes:\n\n", PAGE_SIZE);
+
+    for (size_t i = 0; i < file_size; i += PAGE_SIZE)
+    {
+        fprintf(out,
+                "data[%5zu] address = %p, value = %02X\n",
+                i,
+                (void *)&data[i],
+                (unsigned char)data[i]);
     }
 
+    fclose(out);
     munmap(data, file_size);
     close(fd);
+
+    printf("Output written to: %s\n", output_filename);
 
     return 0;
 }
